ff0ccab2cbaf5a514650e0fc7ae21c80526ad341
pramodnagare <37279055+pramodnagare@users.noreply.github.com>
2018-12-06 14:35:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,99 +1,85 @@
+console.log('Loading updated function');
+var aws = require('aws-sdk');
+var ses = new aws.SES({
+   region: 'us-east-1'
+});
+var ddb = new aws.DynamoDB();
 
 exports.handler = function(event, context, callback) {
-	
-	console.log('Start of Lambda funtion execution.');
-	
-	//Getting AWS SDK object.
-	var aws_sdk = require('aws-sdk');
-	
-	// Getting aws SES service object.
-	var ses = new aws_sdk.SES({region: 'us-east-1'});
-	
-	// Getting AWS DynamoDB service object.
-	var dydb = new aws_sdk.DynamoDB();
-
-	// Logging triggering event.
-    console.log("Lambda triggered by: ", event);
-	
-	// Getting message body from the SNS event.
-    var sns_msg = event.Records[0].Sns.Message;
-	
-	// Splitting the message content for the Email creation.
-    var message=sns_msg.split("|");
-	
-	//Getting parameters from sns messgae body.
-    var email_to=message[0];
-    var domain=message[1];
-	var email_from = "no-reply@"+domain;
-    var db_name=message[2];
-	
-	//Setting up password reset link.
-    var reset_link="";
-
-	// Quering the DynamoDB for record check!
-    var dbQuery = {
-      TableName: db_name,
+    console.log("Incoming: ", event);
+    var msgRaw = event.Records[0].Sns.Message;
+    console.log(msgRaw);
+    var msgArr=msgRaw.split("|");
+    var useremail=msgArr[0];
+    var Src=msgArr[1];
+    Src = "no-reply@"+Src;
+    var DdbTable=msgArr[2];
+    var url=msgArr[1];
+    console.log(useremail);
+    var qParams = {
+      TableName: DdbTable,
       Key: {
-        'id' : {S: email_to}
+        'id' : {S: useremail}
       },
       ExpressionAttributeNames:{
-        "#tt": "Token"
+        "#tt": "ttl"
       },
       ProjectionExpression: '#tt'
     };
 
-  dydb.getItem(dbQuery, function(err, data) {
-    if (err) {
+  ddb.getItem(qParams, function(err, data) {
+      if (err) {
           console.log("Unable to query. Error:", JSON.stringify(err, null, 2));
-      }
-      else{
-        if(Object.keys(data).length === 0 && data.constructor === Object){
-          console.log('Password Reset requested by:', email_to);
-		      
-		      console.log("Generating New Token!")
-		      
-		      const ttl = require('uuid/v1');
-		      
-          console.log("Token Generated: "+ttl());
-		  
+      } else {
+        if(Object.keys(data).length === 0 && data.constructor === Object)
+        {
+          console.log(data);
+          console.log("New token generated");
+          console.log('Hello, Message received from SNS:', useremail);
+
+          console.log('===SENDING EMAIL===');
+          var currDate=new Date();
+          console.log("curr:"+currDate);
+          console.log("currs:"+Math.floor(currDate.getTime()/1000));
+          var ttl=Math.floor(currDate.setMinutes(currDate.getMinutes() + 20)/1000);
+          console.log("ttl:"+ttl);
           var itemParams ={
-            TableName: db_name,
+            TableName: DdbTable,
             Item:{
-              "id":{S:email_to},
-              "Token":{S:ttl()}
+              "id":{S:useremail},
+              "ttl":{N:ttl.toString()}
             }
           };
-		  
-          dydb.putItem(itemParams, function(err) {
+          ddb.putItem(itemParams, function(err) {
           if(err) console.log(err);
           else{
-			  
-            reset_link="http://"+domain+"/reset?email="+email_to+"&token="+ttl()+"/";
-			
-            console.log("reset_link:"+reset_link);
-            var ses_data = {
+            url=url+"/reset?email="+useremail+"&token="+ttl.toString();
+            console.log("url:"+url);
+            var eParams = {
               Destination: {
-                  ToAddresses: [email_to]
+                  ToAddresses: [useremail]
               },
               Message: {
                   Body: {
                       Html: {
                           Charset: 'UTF-8',
-                          Data: '<html><body><b>Hi, Your password reset link:<a href=\"'+reset_link+'" target=\"_blank\">Password Reset Link</a></b></body></html>'
+                          Data: '<html><body><b>Please click here for the password reset:<a href=\"http://'+url+'\" target=\"_blank\">Reset Link</a></b></body></html>'
                       }
                   },
                   Subject: {
-                      Data: "CSYE6225: Reset your password!"
+                      Data: "WebApp: Your reset password link is here!"
                   }
               },
-              Source: email_from
+              Source: Src
             };
-            var email = ses.sendEmail(ses_data, function(err, data){
+            var email = ses.sendEmail(eParams, function(err, data){
                 if(err) console.log(err);
                 else {
-                    console.log("Email sent to user: "+email_to);
-					console.log("The email data was: ")
+                    console.log("===EMAIL SENT===");
                     console.log(data);
+
+                    console.log("EMAIL CODE END");
+                    console.log('EMAIL: ', email);
                     context.succeed(event);
                 }
             });
@@ -101,8 +87,73 @@ exports.handler = function(event, context, callback) {
         });
       }
       else{
-            console.log("The TTL Token is already exists for the user:"+ email_to);
+            console.log("Token already exist.");
+            console.log(data);
+            var tkn_ttl=parseInt((JSON.stringify(data.Item.ttl.N)).replace('\"',''));
+            console.log("tkn_ttl:"+tkn_ttl);
+            var currDate=new Date();
+            var curr_ttl=Math.floor(currDate.getTime()/1000);
+            console.log("curr_ttl:"+curr_ttl);
+            console.log("diff:"+(curr_ttl-tkn_ttl))
+            if((curr_ttl-tkn_ttl)>0)
+            {
+              console.log(data);
+              console.log("New token updated");
+              console.log('Hello, Message received from SNS:', useremail);
+              console.log('===SENDING EMAIL===');
+              var currDate=new Date();
+              console.log("curr:"+currDate);
+              var ttl=Math.floor(currDate.setMinutes(currDate.getMinutes() + 20)/1000);
+              console.log("ttl:"+ttl);
+              var itemParams ={
+                TableName: DdbTable,
+                Item:{
+                  "id":{S:useremail},
+                  "ttl":{N:ttl.toString()}
+                }
+              };
+              ddb.putItem(itemParams, function(err) {
+              if(err) console.log(err);
+              else{
+                 url=url+"/reset?email="+useremail+"&token="+ttl.toString();
+                 console.log("url:"+url);
+                  var eParams = {
+                    Destination: {
+                        ToAddresses: [useremail]
+                    },
+                    Message: {
+                        Body: {
+                            Html: {
+                                Charset: 'UTF-8',
+                                Data: '<html><body><b>Click Here:<a href=\"http://'+url+'\" target=\"_blank\">Reset Link</a></b></body></html>'
+                            }
+                        },
+                        Subject: {
+                            Data: "WebApp: Your reset password link is here!"
+                        }
+                    },
+                    Source: Src
+                };
+                console.log("EMAIL CODE END");
+                var email = ses.sendEmail(eParams, function(err, data){
+                  
+                    
+                    if(err) console.log(err);
+                    else {
+                        console.log("===EMAIL SENT===");
+                        console.log(data);
+
+                        console.log("EMAIL CODE END");
+                        console.log('EMAIL: ', email);
+                        context.succeed(event);
+                    }
+                });
+              }
+            });
+          }
         }
-      
-  }});
-};
+      }
+  });
+
+//    callback(null, "Success");
+}
